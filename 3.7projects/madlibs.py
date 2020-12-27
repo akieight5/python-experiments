b15fd0c5689bbd8cc107258a9f3b3af6f8f0ec3f
akieight5 <alan.r.nicholl@gmail.com>
2020-12-27 17:27:22 +0000

    added a output message

--- 3.7projects/madlibs.py
+++ 3.7projects/madlibs.py
@@ -22,3 +22,4 @@ madlib = f"Computer programming is so {adj}! It makes me so excited all the time
 I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
 
 print(madlib)
+print(adj, verb1, verb2, famous_person)
